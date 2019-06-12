7e0084ba82aabc93b2cdbfd45af9ef80d5732b54
jcm516 <justin.mcdonald9516@gmail.com>
2019-06-12 09:42:35 -0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -23,8 +22,7 @@ import { FooterComponent } from './footer/footer.component';
     HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: QuizComponent }, 
-    ]),
-    BrowserAnimationsModule 
+    ])
   ],
   declarations: [ 
     AppComponent, 
