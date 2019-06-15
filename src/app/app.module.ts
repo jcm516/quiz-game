ac443ab2870ec239b6239c9045a228191c4c6c17
jcm516 <justin.mcdonald9516@gmail.com>
2019-06-15 12:41:37 -0700

    1 file modified, 1 deleted: Added Material dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,40 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { QuizService } from './quiz.service';
@@ -16,18 +47,54 @@ import { FooterComponent } from './footer/footer.component';
 import { LoadingService } from './loading.service';
 
 @NgModule({
+  exports: [
+    // Material
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatNativeDateModule,
+  ]
+})
+export class MaterialModule {}
+
+@NgModule({
   imports:  [ 
     BrowserModule, 
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', component: QuizComponent }, 
-    ])
+    HttpClientModule,    
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   declarations: [ 
-    AppComponent, 
-    HelloComponent, 
+    AppComponent,
     QuizComponent, 
     StatisticsComponent,
     TopBarComponent,
